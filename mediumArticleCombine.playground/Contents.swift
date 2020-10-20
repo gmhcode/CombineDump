e59799ea255dc16e7ab0149d47be20142847cfa7
Greg Hughes <dcgg_101@hotmail.com>
2020-10-20 11:32:15 -0600

    contents changed

---
+++ mediumArticleCombine.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+import Combine
+var str = "Hello, playground"
+
+let publisher = Just("Combine Swift")
+
+
+//let sequencePublisher = let publisher = Publishers.Sequence(sequence: [1,2,3,5,6])
